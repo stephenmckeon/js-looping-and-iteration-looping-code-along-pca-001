6efbd5f55e4646c587c40f92be0d9d0e644300b3
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-06 13:52:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, message) {
+  let array = []
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+  }
+  return array
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number)
+    number--
+  }
+}
